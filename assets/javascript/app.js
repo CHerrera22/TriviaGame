d43ee9cc3c750c8f8cfe68409eb7142c180a0dff
Cris Herrera <cherrera22@gmail.com>
2019-03-26 09:39:28 -0700

    added some code for timer to function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,21 +5,29 @@ var correct = 0;
 var incorrect = 0;
 var unanswered = 0
 
-$("start-button").on("click", function () {
+var intervalID
+
+$("#start-button").on("click", function () {
     startCountdown();
-    return;
 
 });
+
 
 function countdown() {
     count--;
 
-    $('timerNumber').text(count + "Seconds");
+    $('#timerNumber').html(count + " Seconds");
 
+    if (count === 0) {
+        stop();
+
+        alert("Time's Up!");
+     
+    }
 }
 
 function startCountdown() {
-    setInterval(countdown, 1000);
+    intervalID = setInterval(countdown, 1000);
 }
 //i don't think this is the right way to get my timer to operate.
 //not sure how to set the page to check answers once the timer is up. will need to figure that out, 
